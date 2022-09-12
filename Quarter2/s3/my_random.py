788f1f76c0698431363a7a5d242a98d7f885ca87
Alexandr Matanov <xadx@inbox.ru>
2022-09-12 06:18:00 +0300

    add s3 task1 my_random

---
+++ Quarter2/s3/my_random.py
@@ -0,0 +1,22 @@
+from time import *
+
+
+def my_rnd(start, end):
+    n = len(str(end))
+    sup = str(time())[-n:]
+    sup = int(sup)
+
+    if (sup >= start and sup <= end):
+        return sup
+    else:
+        while (sup < start or sup > end):
+            while(sup > end):
+                sup -= end//2
+            while (sup < start):
+                sup += start*2
+        return sup
+
+
+d = [int(x) for x in input('enter min and max separates by space: ').split()]
+r = my_rnd(d[0], d[1])
+print(r)
